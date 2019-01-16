ab9d002f5032805925f4f59fdd894a86368262ce
couturecraigj <couturecraigj@gmail.com>
2019-01-16 16:44:12 -0500

    first week setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Webinars
 
 I intend to hold all my source in this folder for my webinars
+
+## Learning to Program should not be sooooo hard.
+
+this series is intended to help with that.
---
+++ webinars/first-week/index.js
@@ -0,0 +1,71 @@
+// DECLARATIONS
+
+const nonReassigningVariable = 'string';
+
+try {
+  nonReassigningVariable = 'NOT-ALLOWED';
+} catch (error) {
+  
+}
+
+let reAssignableVariable = "can-be changed";
+reAssignableVariable = 'this is allowed';
+
+var generalVariable = 'this is the loosest form of variable';
+
+// Declarations can also be attached to an existing instance
+generalVariable.anInstanceAttribute = 6;
+
+// FUNCTIONS
+
+const addNumbersArrow = (numOne, numTwo) => numOne + numTwo;
+
+function addNumbersFunction (numOne, numTwo) {
+  return numOne + numTwo;
+}
+
+// functions are just like other objects and can have declarations attached as attributes
+
+addNumbersArrow.timesCalled = 0;
+
+const result = (function() {
+  /**
+   * DO SOMETHING ASYNCHRONOUSLY
+   */
+  return 'this is the result';
+})()
+
+// NUMBERS
+
+const one = 1;
+const decimal = 0.2;
+
+// Objects
+
+const object = {
+  attribute: 5,
+  methodOne: () => {
+    console.log('methodOne called');
+  },
+  toJSON() {
+    return 'nothing here!';
+  }
+};
+
+class ClassOne {
+  constructor(data) {
+    this.data = data;
+  }
+  setMethod(data) {
+    this.data = data;
+  }
+  getMethod() {
+    return this.data
+  }
+  typicalMethod(newData) {
+    this.data = newData;
+    return true;
+  }
+}
+
+const object = new ClassOne();
